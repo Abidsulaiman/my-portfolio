8b44137513091d4c7f0b1c39ef5c027c4b178c6a
abidsulaiman <abidqubit@gmail.com>
2023-04-28 02:41:22 +0530

    changes

---
+++ backend/schemas/projects.ts
@@ -0,0 +1,17 @@
+export default {
+    name: 'project',
+    type: 'document',
+      title: 'Project',
+    fields: [      
+        {
+          name: 'image',
+          type: 'image',
+          title: 'Image'
+        },
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      }
+    ]
+  }
